7e366cc3d8e3530d6a3ad437741631cd27d3cb4b
jlindeloef <>
2023-09-08 17:34:27 +0000

    Add color to the buttons and the hovereffects in the game.

--- assets/js/script.js
+++ assets/js/script.js
@@ -91,13 +91,21 @@ function startQuiz() {
     while (answerButtons.firstChild) 
       answerButtons.removeChild(answerButtons.firstChild)
     }
-  function selectAnswer() {
+  function selectAnswer(e) {
     const selectedBtn = e.target;
     const isCorrect = selectedBtn.dataset.correct === "true";
     if(isCorrect) {
-        selectedBtn.classlist.add("correct");
-    }else{
-        selectedBtn.classlist.add(incorrect);
+        selectedBtn.classList.add('correct');
+        score++;
+    }  else  {
+        selectedBtn.classList.add('incorrect');
     }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true") {
+        button.classList.add("correct");
+        }
+        button.disabled = true;
+    })
+    nextButton.style.display = "block";
   }
     startQuiz()
